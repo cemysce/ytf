b4ac68d0c6417f4b6e6a5448e368d6940ab1f371
cemysce <13400533+cemysce@users.noreply.github.com>
2022-11-28 13:14:41 -0500

    Remove unnecessary # from ytf.bat

--- ytf.bat
+++ ytf.bat
@@ -1,8 +1,8 @@
-@REM # Copyright (c) 2022, cemysce
-@REM # All rights reserved.
-@REM #
-@REM # This source code is licensed under the BSD-style license found in the
-@REM # LICENSE file in the root directory of this source tree.
+@REM Copyright (c) 2022, cemysce
+@REM All rights reserved.
+@REM
+@REM This source code is licensed under the BSD-style license found in the
+@REM LICENSE file in the root directory of this source tree.
 
 @echo off
 python "%~dp0ytf"
